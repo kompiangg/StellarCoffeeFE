251f490a316a50a77ec45d0b9b1a13b0bbda39cd
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2021-12-04 16:43:09 +0800

    [#7] create JSON which will send to backend

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -6,89 +6,128 @@
       this.classList.toggle('active-order')
     })
   });
-
 })();
 
+// Cart Feature
 (function() {
-  const btnCart = document.querySelectorAll('.btn-cart');
   const selectedItems = {};
+  const btnCart = document.querySelectorAll('.btn-cart');
+  const quantityAllItems = document.querySelector('.quantity-all-items');
+  const grandTotal = document.querySelector('.grand-total');
+  const checkoutButton = document.querySelector('.checkout-btn');
+  const clearBtn = document.querySelector('.clear-cart-btn');
+
   btnCart.forEach(element => {
     element.addEventListener('click', function() {
       const menuId = parseInt(this.dataset.menuid);
+      let currentTotalQuantity = parseInt(quantityAllItems.children[1].innerHTML);
+      let currentGrandTotal = parseInt(grandTotal.children[1].innerHTML.slice(2))
       
       if (selectedItems[menuId]) {
         const cartContainer = document.querySelector('.container-cart-item');
-        const allItemsCart = cartContainer.querySelector(`[data-menuid="${menuId}"]`)
+        const allItemsCart = cartContainer.querySelector(`[data-menuid="${menuId}"]`);
         
-        selectedItems[menuId].quantity++;
-        allItemsCart.children[0].children[1].children[2].children[0].children[1].innerHTML = selectedItems[menuId].quantity;
-
         selectedItems[menuId].totalPrice += selectedItems[menuId].hargaMenu;
+        allItemsCart.children[0].children[1].children[2].children[0].children[1].innerHTML = ++selectedItems[menuId].quantity;
         allItemsCart.children[0].children[1].children[3].children[0].children[1].innerHTML = `Rp${selectedItems[menuId].totalPrice}`;
       } else {
-        const dataItem = {};
+        const currentItem = {};
         
-        dataItem.menuId = menuId;
-        
-        dataItem.namaMenu = this.parentElement.children[0].innerText;
+        currentItem.menuId = menuId;
+        currentItem.namaMenu = this.parentElement.children[0].innerText;
         
         let hargaMenuString = this.children[1].innerText.slice(2);
         const indexTitik = hargaMenuString.indexOf('.');
         hargaMenuString = hargaMenuString.slice(0,indexTitik);
-        dataItem.hargaMenu = parseInt(hargaMenuString)*1000;
-        dataItem.totalPrice = dataItem.hargaMenu;
-        dataItem.quantity = 1;
+        currentItem.hargaMenu = parseInt(hargaMenuString)*1000;
+        currentItem.totalPrice = currentItem.hargaMenu;
+        currentItem.quantity = 1;
         
         let fullPathGambar = this.parentElement.parentElement.children[0].src;
         const indexTitikGambar = fullPathGambar.indexOf('/assets');
         fullPathGambar = fullPathGambar.slice(indexTitikGambar);
-        dataItem.pathGambar = fullPathGambar;
+        currentItem.pathGambar = fullPathGambar;
         
-        selectedItems[dataItem.menuId] = dataItem;
-  
+        selectedItems[currentItem.menuId] = currentItem;
+        
+        
         const cartItem = document.createElement('div');
-  
         cartItem.classList.add("item-cart");
         cartItem.style.marginBottom = "30px";
-        cartItem.dataset.menuid = dataItem.menuId
-        cartItem.innerHTML = `<div class="row">
-                                <div class="col-md-3 offset-2 text-center">
-                                  <img src="${dataItem.pathGambar}" style="width: 180px;">
-                                </div>
-                                <div class="col-md-5">
-                                  <div class="row">
-                                    <div class="d-flex justify-content-between">
-                                        <h5>Product Name : </h5>
-                                        <p>${dataItem.namaMenu}</p>
-                                    </div>
+        cartItem.dataset.menuid = currentItem.menuId
+        cartItem.innerHTML = `<div class="row text-center">
+        <div class="col-md-2 offset-md-2">
+        <img src="${currentItem.pathGambar}" class="cart-img" style="width: 180px;">
+        </div>
+        <div class="col-md-6">
+        <div class="row">
+        <div class="d-flex justify-content-between">
+        <h5>Product Name : </h5>
+        <p>${currentItem.namaMenu}</p>
+        </div>
+        </div>
+        <div class="row">
+        <div class="d-flex justify-content-between">
+        <h5>Price : </h5>
+        <p>Rp${currentItem.hargaMenu}</p>
+        </div>
                                   </div>
                                   <div class="row">
-                                    <div class="d-flex justify-content-between">
-                                        <h5>Price : </h5>
-                                        <p>Rp${dataItem.hargaMenu}</p>
-                                    </div>
+                                  <div class="d-flex justify-content-between">
+                                  <h5>Quantity : </h5>
+                                  <p>${currentItem.quantity}</p>
+                                  </div>
                                   </div>
                                   <div class="row">
-                                    <div class="d-flex justify-content-between">
-                                        <h5>Quantity : </h5>
-                                        <p>${dataItem.quantity}</p>
-                                    </div>
+                                  <div class="d-flex justify-content-between">
+                                  <h5>Total Price : </h5>
+                                  <p>Rp${currentItem.totalPrice}</p>
                                   </div>
-                                  <div class="row">
-                                    <div class="d-flex justify-content-between">
-                                        <h5>Total Price : </h5>
-                                        <p>Rp${dataItem.totalPrice}</p>
-                                    </div>
                                   </div>
-                                </div>
-                              </div>`
-        
+                                  </div>
+                                  </div>`
+                                  
         const containerCartItem = document.querySelector('.container-cart-item');
-        const quantityAllItem = document.querySelector('.quantity-all-items');
-        containerCartItem.insertBefore(cartItem, quantityAllItem);
-        console.log(selectedItems);
+        const horizontalDivider = document.querySelector('.horizontal-cart-divider');
+        
+        containerCartItem.insertBefore(cartItem, horizontalDivider);
       }
-
+      quantityAllItems.children[1].innerHTML = ++currentTotalQuantity;
+      currentGrandTotal += selectedItems[menuId].hargaMenu;
+      grandTotal.children[1].innerHTML = `Rp${currentGrandTotal}`;
     });
   });
+
+  checkoutButton.addEventListener('click', function() {
+    const allCartItems = document.querySelectorAll('.item-cart');
+    if(Object.keys(selectedItems).length == 0) {
+      alert("Anda belum memesan makanan");
+    } else {
+      const willPOST = {};
+
+      willPOST.allItems = [];
+      Object.keys(selectedItems).forEach(element => {
+        willPOST.allItems.push(selectedItems[String(element)]);
+        console.log(element);
+      });
+      willPOST.quantity = parseInt(quantityAllItems.children[1].innerHTML);
+      willPOST.grandTotal = parseInt(grandTotal.children[1].innerHTML.slice(2));
+      willPOST.done = false;
+      willPOST.idUser = "Coming Soon";
+      willPOST.created = Date.now();
+      console.log(willPOST);
+      deleteCartItems(allCartItems);
+    }
+  });
+
+  clearBtn.addEventListener('click', function () {
+    const allCartItems = document.querySelectorAll('.item-cart');
+    deleteCartItems(allCartItems);
+  });
+
+  function deleteCartItems(allCartItems) {
+    allCartItems.forEach(element => element.remove());
+    quantityAllItems.children[1].innerHTML = 0;
+    grandTotal.children[1].innerHTML = 'Rp0';
+  }
 })();
