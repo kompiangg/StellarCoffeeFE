612848993b852288cf2b07429472cb170f039a69
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2021-12-06 15:20:17 +0800

    [#9] fix bug

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,3 +1,4 @@
+const URL_SERVER = 'http://127.0.0.1:5000';
 (function() {
   const coll = document.querySelectorAll(".collapse-order");
   
@@ -54,38 +55,39 @@
         const cartItem = document.createElement('div');
         cartItem.classList.add("item-cart");
         cartItem.style.marginBottom = "30px";
-        cartItem.dataset.menuid = currentItem.menuId
-        cartItem.innerHTML = `<div class="row text-center">
-        <div class="col-md-2 offset-md-2">
-        <img src="${currentItem.pathGambar}" class="cart-img" style="width: 180px;">
-        </div>
-        <div class="col-md-6">
-        <div class="row">
-        <div class="d-flex justify-content-between">
-        <h5>Product Name : </h5>
-        <p>${currentItem.namaMenu}</p>
-        </div>
-        </div>
-        <div class="row">
-        <div class="d-flex justify-content-between">
-        <h5>Price : </h5>
-        <p>Rp${currentItem.hargaMenu}</p>
-        </div>
-                                  </div>
-                                  <div class="row">
-                                  <div class="d-flex justify-content-between">
-                                  <h5>Quantity : </h5>
-                                  <p>${currentItem.quantity}</p>
-                                  </div>
-                                  </div>
-                                  <div class="row">
-                                  <div class="d-flex justify-content-between">
-                                  <h5>Total Price : </h5>
-                                  <p>Rp${currentItem.totalPrice}</p>
-                                  </div>
-                                  </div>
-                                  </div>
-                                  </div>`
+        cartItem.dataset.menuid = currentItem.menuId;
+        cartItem.innerHTML = 
+        `<div class="row text-center">
+          <div class="col-md-2 offset-md-2">
+            <img src="${currentItem.pathGambar}" class="cart-img" style="width: 180px;">
+          </div>
+          <div class="col-md-6">
+            <div class="row">
+              <div class="d-flex justify-content-between">
+                <h5>Product Name : </h5>
+                <p>${currentItem.namaMenu}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="d-flex justify-content-between">
+                <h5>Price : </h5>
+                <p>Rp${currentItem.hargaMenu}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="d-flex justify-content-between">
+                <h5>Quantity : </h5>
+                <p>${currentItem.quantity}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="d-flex justify-content-between">
+                <h5>Total Price : </h5>
+                <p>Rp${currentItem.totalPrice}</p>
+              </div>
+            </div>
+          </div>
+        </div>`
                                   
         const containerCartItem = document.querySelector('.container-cart-item');
         const horizontalDivider = document.querySelector('.horizontal-cart-divider');
@@ -100,23 +102,40 @@
 
   checkoutButton.addEventListener('click', function() {
     const allCartItems = document.querySelectorAll('.item-cart');
+    console.log(selectedItems);
     if(Object.keys(selectedItems).length == 0) {
       alert("Anda belum memesan makanan");
     } else {
       const willPOST = {};
 
       willPOST.allItems = [];
-      Object.keys(selectedItems).forEach(element => {
-        willPOST.allItems.push(selectedItems[String(element)]);
-        console.log(element);
-      });
+      Object.keys(selectedItems).forEach(element => { willPOST.allItems.push(selectedItems[String(element)]) });
       willPOST.quantity = parseInt(quantityAllItems.children[1].innerHTML);
       willPOST.grandTotal = parseInt(grandTotal.children[1].innerHTML.slice(2));
       willPOST.done = false;
       willPOST.idUser = "Coming Soon";
       willPOST.created = Date.now();
-      console.log(willPOST);
-      deleteCartItems(allCartItems);
+
+      fetch(URL_SERVER+"/api/order/checkout", {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          'SC-API-TOKEN': 'KyaanDameNakaWaZettaiDameeDaaa',
+        },
+        body: JSON.stringify(willPOST),
+      })
+        .then(response => response.json())
+        .then(response => {
+          if (response.status == "OK") {
+            alert("Terima kasih terlah berbelanja, silahkan tunggu pesanan anda");
+          } else {
+            alert(`ERROR: Something went wrong (${response})`);
+          }
+        })
+        .catch(error => alert(`ERROR: Something went wrong (${error})`));
+      
+        deleteCartItems(allCartItems);
     }
   });
 
@@ -124,10 +143,13 @@
     const allCartItems = document.querySelectorAll('.item-cart');
     deleteCartItems(allCartItems);
   });
-
+  
   function deleteCartItems(allCartItems) {
-    allCartItems.forEach(element => element.remove());
+    allCartItems.forEach(element => {element.remove()} );
     quantityAllItems.children[1].innerHTML = 0;
     grandTotal.children[1].innerHTML = 'Rp0';
+    Object.keys(selectedItems).forEach(e => {
+      delete selectedItems[`${e}`];
+    });
   }
 })();
