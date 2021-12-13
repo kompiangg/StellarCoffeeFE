fc6c17ba0ac1abcc47cd0db3d723b4cf7015747d
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2021-12-14 02:37:33 +0800

    add logout button

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -3,11 +3,22 @@
   const header = document.querySelector('#navbar-button');
   const username = localStorage.getItem('username');
   if(username) {
-    navbarButton = `<a href="personal-page.html" class="book-a-table-btn scrollto d-none d-lg-flex login-button"> <i class="bi bi-person-fill" style="padding-right:10px;padding-left:0px" ></i> ${username}</a>`;
+    navbarButton = `
+                    <a href="personal-page.html" class="book-a-table-btn scrollto d-none d-lg-flex login-button"> <i class="bi bi-person-fill" style="padding-right:10px;padding-left:0px" ></i> ${username}</a>
+                    <a class="book-a-table-btn scrollto d-none d-lg-flex logout-button">Logout</a>`;
   } else {
     navbarButton = `
                     <a href="login.html" class="book-a-table-btn scrollto d-none d-lg-flex login-button">Login</a>
                     <a href="sign-up.html" class="book-a-table-btn scrollto d-none d-lg-flex sign-up-button"> Sign-Up</strong> </a>`;
   }
   header.innerHTML = navbarButton;
+})();
+
+(function() {
+  document.addEventListener('click', element => {
+    if (element.target.classList.contains('logout-button')) {
+      localStorage.clear();
+      window.location.replace('index.html');
+    }
+  })
 })();
